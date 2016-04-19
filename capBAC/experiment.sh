7ef70e32fd0300976d4eb7e81aa6b7c1384a487f
Sajin <sajin.sasy@gmail.com>
2016-04-18 23:19:03 -0400

    Experiment Tweaking

--- capBAC/experiment.sh
+++ capBAC/experiment.sh
@@ -21,13 +21,13 @@ BASE_PORT=49180
 PORT=$BASE_PORT
 let MAX_PORT=$BASE_PORT+$N
 until [ $PORT -ge $MAX_PORT ]; do
-    ./capbac verify 1 $PORT &
+    ./capbac verify 2 $PORT &
     VPIDS[$PORT]=$!
     let PORT=$PORT+1
 done
 
 # start 1 authority
-./auth 1 49151 >&1 logs/auth_log &
+./auth 2 49151 49180 >&1 logs/auth_log &
 APID=$!
 
 # start m clients
@@ -35,10 +35,10 @@ M=5
 START=`date +%s`
 CLOCK_TIME=5 # time in seconds to run requests
 echo running requests for $CLOCK_TIME seconds...
-./client <(echo createc c1 && yes access 49180 token.json) 49151 1 > logs/client_log 2>&1 &
-./client <(echo createc c2 && yes access 49181 token.json) 49151 1 > logs/client_log 2>&1 &
-./client <(echo createc c3 && yes access 49182 token.json) 49151 1 > logs/client_log 2>&1 &
-#./client <(echo createc c2 && yes access 49180 token.json $'\n'access 49181 token.json $'\n'access 49182 token.json) 49151 > logs/client_log 2>&1 &
+./client <(echo createc c1 && yes access 49180 token.json) 49151 2 > logs/client_log 2>&1 &
+./client <(echo createc c2 && yes access 49181 token.json) 49151 2 > logs/client_log 2>&1 &
+#./client <(echo createc c3 && yes access 49182 token.json) 49151 1 > logs/client_log 2>&1 &
+#./client <(echo createc c3 && yes access 49180 token.json $'\n'access 49181 token.json $'\n'access 49182 token.json) 49151 2 > logs/client_log 2>&1 &
 #./client commands 49151 > /dev/null 2>&1 &
 sleep $CLOCK_TIME
 
