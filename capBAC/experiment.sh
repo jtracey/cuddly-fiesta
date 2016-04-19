72d57644e9c402ad1693916641c57b6db7919bd8
Sajin <sajin.sasy@gmail.com>
2016-04-18 20:16:09 -0400

    script changes

--- capBAC/experiment.sh
+++ capBAC/experiment.sh
@@ -17,7 +17,7 @@ function testmode {
 
 # generate n verifiers
 N=3
-BASE_PORT=49152
+BASE_PORT=49180
 PORT=$BASE_PORT
 let MAX_PORT=$BASE_PORT+$N
 until [ $PORT -ge $MAX_PORT ]; do
@@ -35,8 +35,10 @@ M=5
 START=`date +%s`
 CLOCK_TIME=5 # time in seconds to run requests
 echo running requests for $CLOCK_TIME seconds...
-./client <(echo createc file2.txt && yes access 49152 token.json) 49151 1 > logs/client_log 2>&1 &
-#./client <(echo createc sajin && yes access 49152 token.json $'\n'access 49153 token.json $'\n'access 49154 token.json) 49151 > logs/client_log 2>&1 &
+./client <(echo createc c1 && yes access 49180 token.json) 49151 1 > logs/client_log 2>&1 &
+./client <(echo createc c2 && yes access 49181 token.json) 49151 1 > logs/client_log 2>&1 &
+./client <(echo createc c3 && yes access 49182 token.json) 49151 1 > logs/client_log 2>&1 &
+#./client <(echo createc c2 && yes access 49180 token.json $'\n'access 49181 token.json $'\n'access 49182 token.json) 49151 > logs/client_log 2>&1 &
 #./client commands 49151 > /dev/null 2>&1 &
 sleep $CLOCK_TIME
 
